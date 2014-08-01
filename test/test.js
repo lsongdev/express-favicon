21c247c35cfd62303293fe072837e89a34385513
Lsong <song940@gmail.com>
2014-08-01 12:29:39 +0800

    more test .

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var favicon = require('../index');
+var http = require('http');
 
 var app = express();
 
@@ -8,4 +9,16 @@ app.use(favicon(__dirname + '/favicon.ico'));
 
 app.listen(3001, function(){
 	console.log('server is running .');
+	http.get('http://127.0.0.1:3001/blabla/favicon.ico', function(res){
+		var data = '';
+		res.on('data', function(chunk){
+			data += chunk;
+		});
+		res.on('end', function(){
+			var is_ok = (data.trim() == 'test');
+			console.log('test: %s', is_ok);
+			process.exit(is_ok ? 0 : 1);
+		});
+	});
+	
 });
