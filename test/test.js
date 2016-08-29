534b054ee8a60be4453b014d03bad72ba69c7380
Jannis R <mail@jannisr.de>
2016-08-30 01:47:17 +0200

    make tests more robust
    Using `*` as Express version allows new, non-backwards-compatible version Express to break the build.
    
    It is safer to compare the favicon.ico content bytewise, using buffers.
    
    Using the assert module yields in nice-to-read stack traces & nice-to-read tests.
    
    The test script lets HTTP request errors fail the test now.

--- test/test.js
+++ test/test.js
@@ -1,25 +1,30 @@
 'use strict'
 
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const favicon = require('../index');
 const http = require('http');
+const assert = require('assert');
+
+const file = path.join(__dirname, 'favicon.ico');
+const data = fs.readFileSync(file);
 
 const app = express();
-
-app.use(favicon(__dirname + '/favicon.ico'));
+app.use(favicon(file));
 
 
 app.listen(3001, () => {
-	console.log('server is running .');
+	console.info('server is running.');
 	http.get('http://127.0.0.1:3001/blabla/favicon.ico', (res) => {
-		let data = '';
-		res.on('data', (chunk) => {
-			data += chunk;
-		});
+		let received = [];
+		res.on('data', (chunk) => received.push(chunk));
 		res.on('end', () => {
-			const is_ok = data.trim() === 'test';
-			console.log('test: %s', is_ok);
-			process.exit(is_ok ? 0 : 1);
+			const isEqual = Buffer.compare(data, Buffer.concat(received)) === 0;
+			assert.ok(isEqual, 'received data is not equal');
+			console.info('test passed.');
+			process.exit(0);
 		});
-	});
+	})
+	.on('error', assert.ifError);
 });
