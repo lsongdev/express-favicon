a5c990231599f660350885fcd9f24a74470b24da
Jannis R <mail@jannisr.de>
2016-08-30 01:22:08 +0200

    update to ES6 syntax
    As Node 4 (LTS right now) supports the necessary ES6 features, I updated the source code and tests to leverage more concise & safe declarations. Travis CI will now run on NODE 4, 5, 6.

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-var fs = require('fs');
+'use strict'
 
-module.exports = function(icon){
-	var iconStream = null;
+const fs = require('fs');
+
+module.exports = (icon) => {
+	let iconStream = null;
 	if(fs.existsSync(icon)){
 		iconStream = fs.readFileSync(icon);
 	}else{
 		throw new Error('favicon.ico is not found .');
 	}
-	return function(req, res, next){
+	return (req, res, next) => {
 		if(/\/favicon\.ico$/.test(req.url)){
 			res.set('Content-Type', 'image/x-icon');
 			res.send(iconStream);
--- test/test.js
+++ test/test.js
@@ -1,24 +1,25 @@
-var express = require('express');
-var favicon = require('../index');
-var http = require('http');
+'use strict'
 
-var app = express();
+const express = require('express');
+const favicon = require('../index');
+const http = require('http');
+
+const app = express();
 
 app.use(favicon(__dirname + '/favicon.ico'));
 
 
-app.listen(3001, function(){
+app.listen(3001, () => {
 	console.log('server is running .');
-	http.get('http://127.0.0.1:3001/blabla/favicon.ico', function(res){
-		var data = '';
-		res.on('data', function(chunk){
+	http.get('http://127.0.0.1:3001/blabla/favicon.ico', (res) => {
+		let data = '';
+		res.on('data', (chunk) => {
 			data += chunk;
 		});
-		res.on('end', function(){
-			var is_ok = (data.trim() == 'test');
+		res.on('end', () => {
+			const is_ok = data.trim() === 'test';
 			console.log('test: %s', is_ok);
 			process.exit(is_ok ? 0 : 1);
 		});
 	});
-	
 });
