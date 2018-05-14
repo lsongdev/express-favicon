5c096146a5216c7453a67a3e0eb837a7638cf648
Lsong <song940@gmail.com>
2018-05-14 19:38:52 +0800

    modify indent

--- test/test.js
+++ test/test.js
@@ -14,18 +14,18 @@ app.use(favicon(file));
 
 const server = http.createServer(app);
 server.listen(3001, () => {
-	console.info('server is running.');
-	http.get('http://127.0.0.1:3001/blabla/favicon.png', (res) => {
-			assert.equal(res.headers['content-type'], 'image/png', 'wrong mime type');
-			let received = [];
-			res.on('data', (chunk) => received.push(chunk));
-			res.on('end', () => {
+ console.info('server is running.');
+ http.get('http://127.0.0.1:3001/blabla/favicon.png', (res) => {
+   assert.equal(res.headers['content-type'], 'image/png', 'wrong mime type');
+   let received = [];
+   res.on('data', (chunk) => received.push(chunk));
+   res.on('end', () => {
         const data = fs.readFileSync(file);
-				const isEqual = Buffer.compare(data, Buffer.concat(received)) === 0;
-				assert.ok(isEqual, 'received data is not equal');
-				console.info('test passed.');
-				server.close((() => process.exit(0)));
-			});
-		})
-		.on('error', assert.ifError);
+    const isEqual = Buffer.compare(data, Buffer.concat(received)) === 0;
+    assert.ok(isEqual, 'received data is not equal');
+    console.info('test passed.');
+    server.close((() => process.exit(0)));
+   });
+  })
+  .on('error', assert.ifError);
 });
